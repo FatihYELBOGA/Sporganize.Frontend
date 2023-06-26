b85668ca955a83b515746de81d6fdd2894b86401
merveozan <merveozan581@gmail.com>
2023-06-27 01:26:46 +0300

    yerlerini düzenledim
    yerlerini düzenledim

--- src/components/Users/Profile/profile.js
+++ src/components/Users/Profile/profile.js
@@ -4,7 +4,6 @@ import {
   Select, MenuItem, InputLabel, FormControl, Button, createTheme, ThemeProvider
 } from '@mui/material';
 
-// Simulating provinces, districts and streets data
 const addressData = {
   "İstanbul": {
     "Kadıköy": ["Acıbadem", "Moda", "Erenköy"],
@@ -25,6 +24,37 @@ const theme = createTheme({
       main: '#ffffff',
     }
   },
+  components: {
+    MuiInput: {
+      styleOverrides: {
+        underline: {
+          "&:hover:not(.Mui-disabled):before": {
+            borderBottom: "none"
+          },
+          "&.Mui-focused:after": {
+            borderBottom: "none"
+          }
+        },
+        root: {
+          "&:hover": {
+            backgroundColor: "transparent"
+          },
+          "&:focus": {
+            backgroundColor: "transparent"
+          }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "&.Mui-focused fieldset": {
+            borderColor: "transparent"
+          }
+        }
+      }
+    }
+  }
 });
 
 const Profile = () => {
@@ -39,6 +69,7 @@ const Profile = () => {
   const provinces = Object.keys(addressData);
   const districts = province ? Object.keys(addressData[province]) : [];
   const streets = district ? addressData[province][district] : [];
+  const genders = ["Female","Male"];
 
   const handleProvinceChange = (event) => {
     setProvince(event.target.value);
@@ -64,42 +95,45 @@ const Profile = () => {
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ padding: 3, marginTop: 2, borderRadius: 2, width: '60%', marginLeft: '30%', marginRight: '10%' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', transform: 'rotate(-0.392deg)', flexShrink: 0, marginBottom: 3 }}>
-          <Box sx={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center',justifyContent: 'flex-start',paddingRight: '50%'}}>
-            <Avatar sx={{ width: 80, height: 80, cursor: 'pointer' }} src={avatarSrc} onClick={() => document.getElementById('avatar-upload').click()}/>
-              <Typography variant="h6" component="p" sx={{ color: '#000', textAlign: 'center', textShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)', fontFamily: 'Poppins', fontWeight: 300, fontSize: '32px' }}>John Doe</Typography>
-                <Button variant="contained" color="primary" size="small" sx={{ mt: 1 }}>Edit Profile Photo</Button>
-          </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', marginBottom: 3 }}>
+          <input
+              accept="image/*"
+              id="avatar-upload"
+              type="file"
+              style={{ display: "none" }}
+              onChange={handleAvatarChange}
+            />
+          <Avatar sx={{ width: 80, height: 80, cursor: 'pointer' }} src={avatarSrc} onClick={() => document.getElementById('avatar-upload').click()}/>
+          <Typography variant="h6" component="p" sx={{ color: '#000', textAlign: 'center', textShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)', fontFamily: 'Poppins', fontWeight: 300, fontSize: '32px' }}>John Doe</Typography>
+          <Button variant="contained" color="primary" size="small" sx={{ mt: 1 }} onClick={() => document.getElementById('avatar-upload').click()}>Edit Profile Photo</Button>
        </Box>
         <Grid container spacing={2} sx={{ mt: 3 }}>
-          <Grid item xs={12}>
-            <TextField label="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} fullWidth InputProps={{ style: { color: "#000000" }}} sx={{ width: '50%' }}/>
+          <Grid item xs={6}>
+            <TextField label="First Name" placeholder="Enter your first name" value={firstName} onChange={(e) => setFirstName(e.target.value)} fullWidth/>
           </Grid>
-          <Grid item xs={12}>
-            <TextField label="LastName" value={lastName} onChange={(e) => setLastName(e.target.value)} fullWidth InputProps={{ style: { color: "#000000" }}} sx={{ width: '50%' }}/>
+          <Grid item xs={6}>
+            <TextField label="Last Name" placeholder="Enter your last name" value={lastName} onChange={(e) => setLastName(e.target.value)} fullWidth/>
           </Grid>
-          <Grid item xs={4}>
-            <FormControl fullWidth>
-              <InputLabel id="province-label">Gender</InputLabel>
-              <Select labelId="province-label" id="province-select" value={province} onChange={handleProvinceChange}>
-                {provinces.map((prov, index) => (
-                  <MenuItem key={index} value={prov}>{prov}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField label="Birth Date" type="date" defaultValue=""
+          <Grid item xs={6}>
+            <TextField label="Birth Date" type="date" defaultValue="" placeholder="Enter your birth date"
               value={birthDate}
               onChange={(e) => setBirthDate(e.target.value)}
               InputLabelProps={{
                 shrink: true,
               }}
               fullWidth
-              InputProps={{ style: { color: "#000000" }}}
-              sx={{ width: '50%' }}
             />
           </Grid>  
+          <Grid item xs={6}>
+            <FormControl fullWidth>
+              <InputLabel id="gender-label">Gender</InputLabel>
+              <Select labelId="gender-label" id="gender-select" value={gender} onChange={(e) => setGender(e.target.value)}>
+                {genders.map((gend, index) => (
+                  <MenuItem key={index} value={gend}>{gend}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
           <Grid item xs={4}>
             <FormControl fullWidth>
               <InputLabel id="province-label">Province</InputLabel>
@@ -132,7 +166,7 @@ const Profile = () => {
           </Grid>
         </Grid>
         <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
-          <Button variant="contained" color="primary" sx={{marginRight: '50%', width: '50%' }} onClick={handleSaveChanges}>Save Changes</Button>
+          <Button variant="contained" color="primary" onClick={handleSaveChanges}>Save Changes</Button>
         </Box>
       </Box>
     </ThemeProvider>
